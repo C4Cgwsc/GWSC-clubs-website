2bfe43d55b21249992c20bd48a060fad4966720c
OckTheOctopus <Yaseen6607@yahoo.com>
2021-10-15 17:24:02 +1100

    Added C4C files

---
+++ src/Components/Pages/Clubs/ChatForChange.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// Components
+import {Header} from '../../Header.js';
+import {Footer} from '../../Footer.js';
+import {NavBar} from '../../NavBar.js';
+import {SideBar} from '../../SideBar.js';
+// Styles
+import './ChatForChange.css';
