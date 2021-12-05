ff954cf866142006fc8aed1928527041e2611918
OckTheOctopus <Yaseen6607@yahoo.com>
2021-12-05 16:10:00 +1100

    Un-broke random clubs feature

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 // Styles
 import './SideBar.css';
@@ -6,8 +6,16 @@ import './SideBar.css';
 export const SideBar = () => {
   return (
     <div>
-      <h3>Suggested:</h3>
+      <h3>Check them out!</h3>
       <Link to='/clubs/chatforchange'>Chat for Change</Link>
+      <br />
+      <Link to='/clubs/communityartscouncil'>Community Arts Council</Link>
+      <br />
+      <Link to='/clubs/techcrew'>Tech Crew</Link>
+      <br />
+      <Link to='/clubs/fcsc'>FCSC</Link>
+      <br />
+      <Link to='/clubs/roboticsclub'>Robotics Club</Link>
     </div>
   );
 }
